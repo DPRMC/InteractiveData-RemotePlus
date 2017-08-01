76901c8c42e1772fa4c39bdd7389f78be721a268
Michael Drennen <michaeldrennen74@gmail.com>
2017-08-01 10:34:49 -0600

    Added code to test for pruning of invalid cusips.

--- tests/ClientDailyPriceFixedIncomeTest.php
+++ tests/ClientDailyPriceFixedIncomeTest.php
@@ -8,19 +8,50 @@ use PHPUnit\Framework\TestCase;
 
 class ClientDailyPriceFixedIncomeTest extends TestCase {
 
+    protected $invalidUser = 'user';
+
+    protected $invalidPass = 'pass';
+
+    protected $validDate = '2017-07-31';
+
+    protected $invalidDate = 'this is not a date';
+
+    protected $validCusips = [ '38259P508' ]; // Google
+
+    protected $invalidCusips = [ '123456789' ];
+
+    /**
+     *
+     */
     public function testConstructor() {
-        $client = new ClientDailyPriceFixedIncome( 'user', 'pass', '2017-07-31', [ '38259P508' ], FALSE );
+        $client = new ClientDailyPriceFixedIncome( $this->invalidUser, $this->invalidPass, $this->validDate, $this->validCusips, FALSE );
         $this->assertInstanceOf( ClientDailyPriceFixedIncome::class, $client );
+        $this->assertAttributeCount( 1, 'cusips', $client );
     }
 
+    /**
+     *
+     */
     public function testConstructorWithUnparsableDate() {
         $this->expectException( UnparsableDateSentToConstructor::class );
-        new ClientDailyPriceFixedIncome( 'user', 'pass', 'this is not a date', [ '38259P508' ], FALSE );
+        new ClientDailyPriceFixedIncome( $this->invalidUser, $this->invalidPass, $this->invalidDate, $this->validCusips, FALSE );
     }
 
+    /**
+     *
+     */
+    public function testConstructorWithInvalidCusip() {
+        $client = new ClientDailyPriceFixedIncome( $this->invalidUser, $this->invalidPass, $this->validDate, $this->invalidCusips, FALSE );
+        $this->assertAttributeCount( 0, 'cusips', $client );
+        $this->assertAttributeCount( 1, 'invalidCusips', $client );
+    }
+
+    /**
+     *
+     */
     public function testRunWithInvalidCredentials() {
         $this->expectException( ClientException::class );
-        $client   = new ClientDailyPriceFixedIncome( 'user', 'pass', '2017-07-31', [ '38259P508' ], FALSE );
-        $response = $client->run();
+        $client = new ClientDailyPriceFixedIncome( $this->invalidUser, $this->invalidPass, $this->validDate, $this->validCusips, FALSE );
+        $client->run();
     }
 }
